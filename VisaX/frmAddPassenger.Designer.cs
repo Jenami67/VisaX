488980a7e2138537250711bfcb3ebd8fc9170c88
Mahmood <Mahmood@JENAMI>
2019-02-01 17:11:09 +0330

    Add project files.

---
+++ VisaX/frmAddPassenger.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace VisaX
+{
+    partial class frmAddPassenger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtFamily = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFather = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassportNum = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.txtBornDate = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtIssueDate = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtExpiryDate = new System.Windows.Forms.MaskedTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblBornDate = new System.Windows.Forms.Label();
+            this.lblIssueDate = new System.Windows.Forms.Label();
+            this.lblExpiryDate = new System.Windows.Forms.Label();
+            this.erp = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.erp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "اسم";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(17, 31);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(177, 27);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtFamily
+            // 
+            this.txtFamily.Location = new System.Drawing.Point(218, 31);
+            this.txtFamily.Name = "txtFamily";
+            this.txtFamily.Size = new System.Drawing.Size(177, 27);
+            this.txtFamily.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "فامیل";
+            // 
+            // txtFather
+            // 
+            this.txtFather.Location = new System.Drawing.Point(418, 31);
+            this.txtFather.Name = "txtFather";
+            this.txtFather.Size = new System.Drawing.Size(177, 27);
+            this.txtFather.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(421, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "اسم پدر";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 82);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "شماره پاسپورت";
+            // 
+            // txtPassportNum
+            // 
+            this.txtPassportNum.Location = new System.Drawing.Point(17, 105);
+            this.txtPassportNum.Mask = "00000000";
+            this.txtPassportNum.Name = "txtPassportNum";
+            this.txtPassportNum.Size = new System.Drawing.Size(119, 27);
+            this.txtPassportNum.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(221, 82);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 19);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "جنسیت";
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Items.AddRange(new object[] {
+            "مرد",
+            "زن"});
+            this.cmbGender.Location = new System.Drawing.Point(218, 105);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(121, 27);
+            this.cmbGender.TabIndex = 8;
+            // 
+            // txtBornDate
+            // 
+            this.txtBornDate.Location = new System.Drawing.Point(17, 173);
+            this.txtBornDate.Mask = "00/00/0000";
+            this.txtBornDate.Name = "txtBornDate";
+            this.txtBornDate.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtBornDate.Size = new System.Drawing.Size(119, 27);
+            this.txtBornDate.TabIndex = 10;
+            this.txtBornDate.ValidatingType = typeof(System.DateTime);
+            this.txtBornDate.TypeValidationCompleted += new System.Windows.Forms.TypeValidationEventHandler(this.txtBornDate_TypeValidationCompleted);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 150);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 19);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "تاریخ تولد";
+            // 
+            // txtIssueDate
+            // 
+            this.txtIssueDate.Location = new System.Drawing.Point(218, 173);
+            this.txtIssueDate.Mask = "00/00/0000";
+            this.txtIssueDate.Name = "txtIssueDate";
+            this.txtIssueDate.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtIssueDate.Size = new System.Drawing.Size(119, 27);
+            this.txtIssueDate.TabIndex = 12;
+            this.txtIssueDate.ValidatingType = typeof(System.DateTime);
+            this.txtIssueDate.TypeValidationCompleted += new System.Windows.Forms.TypeValidationEventHandler(this.txtIssueDate_TypeValidationCompleted);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(221, 150);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 19);
+            this.label7.TabIndex = 11;
+            this.label7.Text = " صدور پاسپورت";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(17, 247);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(122, 42);
+            this.btnOK.TabIndex = 13;
+            this.btnOK.Text = "ثبت";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // txtExpiryDate
+            // 
+            this.txtExpiryDate.Location = new System.Drawing.Point(418, 173);
+            this.txtExpiryDate.Mask = "00/00/0000";
+            this.txtExpiryDate.Name = "txtExpiryDate";
+            this.txtExpiryDate.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtExpiryDate.Size = new System.Drawing.Size(119, 27);
+            this.txtExpiryDate.TabIndex = 15;
+            this.txtExpiryDate.ValidatingType = typeof(System.DateTime);
+            this.txtExpiryDate.TypeValidationCompleted += new System.Windows.Forms.TypeValidationEventHandler(this.txtExpiryDate_TypeValidationCompleted);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(421, 150);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(110, 19);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "انقضاء پاسپورت";
+            // 
+            // lblBornDate
+            // 
+            this.lblBornDate.AutoSize = true;
+            this.lblBornDate.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBornDate.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblBornDate.Location = new System.Drawing.Point(63, 203);
+            this.lblBornDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBornDate.Name = "lblBornDate";
+            this.lblBornDate.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblBornDate.Size = new System.Drawing.Size(0, 14);
+            this.lblBornDate.TabIndex = 16;
+            // 
+            // lblIssueDate
+            // 
+            this.lblIssueDate.AutoSize = true;
+            this.lblIssueDate.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIssueDate.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblIssueDate.Location = new System.Drawing.Point(261, 203);
+            this.lblIssueDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIssueDate.Name = "lblIssueDate";
+            this.lblIssueDate.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblIssueDate.Size = new System.Drawing.Size(0, 14);
+            this.lblIssueDate.TabIndex = 17;
+            // 
+            // lblExpiryDate
+            // 
+            this.lblExpiryDate.AutoSize = true;
+            this.lblExpiryDate.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExpiryDate.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblExpiryDate.Location = new System.Drawing.Point(461, 203);
+            this.lblExpiryDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblExpiryDate.Name = "lblExpiryDate";
+            this.lblExpiryDate.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblExpiryDate.Size = new System.Drawing.Size(0, 14);
+            this.lblExpiryDate.TabIndex = 18;
+            // 
+            // erp
+            // 
+            this.erp.ContainerControl = this;
+            this.erp.RightToLeft = true;
+            // 
+            // frmAddPassenger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 301);
+            this.Controls.Add(this.lblExpiryDate);
+            this.Controls.Add(this.lblIssueDate);
+            this.Controls.Add(this.lblBornDate);
+            this.Controls.Add(this.txtExpiryDate);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtIssueDate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtBornDate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.txtPassportNum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFather);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFamily);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmAddPassenger";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "افزودن متقاضی";
+            ((System.ComponentModel.ISupportInitialize)(this.erp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtFamily;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFather;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox txtPassportNum;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.MaskedTextBox txtBornDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox txtIssueDate;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.MaskedTextBox txtExpiryDate;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblBornDate;
+        private System.Windows.Forms.Label lblIssueDate;
+        private System.Windows.Forms.Label lblExpiryDate;
+        private System.Windows.Forms.ErrorProvider erp;
+    }
+}
---
+++ VisaX/frmAddPassenger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace VisaX
+{
+    public partial class frmAddPassenger : Form
+    {
+        private PersianCalendar pc = new PersianCalendar();
+        private DateTime dt;
+
+        public frmAddPassenger()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBornDate_TypeValidationCompleted(object sender, TypeValidationEventArgs e)
+        {
+            if (e.IsValidInput)
+            {
+                dt = DateTime.Parse(txtBornDate.Text);
+                lblBornDate.Text = string.Format("{0}/{1:00}/{2:00}", pc.GetYear(dt), pc.GetMonth(dt), pc.GetDayOfMonth(dt));
+                erp.Clear();
+            }//if
+            else
+            {
+                erp.SetError(txtBornDate, "تاریخ معتبر نیست!");
+                e.Cancel = true;
+            }//else
+        }
+
+        private void txtIssueDate_TypeValidationCompleted(object sender, TypeValidationEventArgs e)
+        {
+            if (e.IsValidInput)
+            {
+                dt = DateTime.Parse(txtIssueDate.Text);
+                lblIssueDate.Text = string.Format("{0}/{1:00}/{2:00}", pc.GetYear(dt), pc.GetMonth(dt), pc.GetDayOfMonth(dt));
+                erp.Clear();
+            }//if
+            else
+            {
+                erp.SetError(txtIssueDate, "تاریخ معتبر نیست!");
+                e.Cancel = true;
+            }//else
+        }
+
+        private void txtExpiryDate_TypeValidationCompleted(object sender, TypeValidationEventArgs e)
+        {
+            if (e.IsValidInput)
+            {
+                dt = DateTime.Parse(txtExpiryDate.Text);
+                lblExpiryDate.Text = string.Format("{0}/{1:00}/{2:00}", pc.GetYear(dt), pc.GetMonth(dt), pc.GetDayOfMonth(dt));
+                erp.Clear();
+            }//if
+            else
+            {
+                erp.SetError(txtExpiryDate, "تاریخ معتبر نیست!");
+                e.Cancel = true;
+            }//else
+        }
+    }
+}
