f652d6bb7c8e76dff1d38326a5c21fee3bb446db
Mahmood <Mahmood@JENAMI>
2019-03-03 22:27:26 +0330

    khordeKari

--- VisaX/frmMain.Designer.cs
+++ VisaX/frmMain.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.dgvPassengers = new System.Windows.Forms.DataGridView();
             this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -41,14 +41,15 @@
             this.colExpiryDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colGender = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnExportPDF = new System.Windows.Forms.Button();
             this.btnExportExcel = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnNew = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.txtFilter = new System.Windows.Forms.TextBox();
-            this.btnExportPDF = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPassengers)).BeginInit();
             this.panel1.SuspendLayout();
@@ -78,8 +79,8 @@
             // 
             this.dgvPassengers.AllowUserToAddRows = false;
             this.dgvPassengers.AllowUserToDeleteRows = false;
-            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(2);
-            this.dgvPassengers.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(2);
+            this.dgvPassengers.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle2;
             this.dgvPassengers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvPassengers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvPassengers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -99,6 +100,8 @@
             this.dgvPassengers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvPassengers.Size = new System.Drawing.Size(939, 352);
             this.dgvPassengers.TabIndex = 0;
+            this.dgvPassengers.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dgvPassengers_RowsAdded);
+            this.dgvPassengers.RowsRemoved += new System.Windows.Forms.DataGridViewRowsRemovedEventHandler(this.dgvPassengers_RowsRemoved);
             // 
             // colID
             // 
@@ -168,7 +171,6 @@
             // panel1
             // 
             this.panel1.Controls.Add(this.btnExportPDF);
-            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.btnExportExcel);
             this.panel1.Controls.Add(this.btnDelete);
             this.panel1.Controls.Add(this.btnEdit);
@@ -179,24 +181,24 @@
             this.panel1.Size = new System.Drawing.Size(939, 64);
             this.panel1.TabIndex = 16;
             // 
-            // button2
+            // btnExportPDF
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(154, 9);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(142, 45);
-            this.button2.TabIndex = 20;
-            this.button2.Text = "ویرایش اکسل";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnExportPDF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExportPDF.Location = new System.Drawing.Point(303, 9);
+            this.btnExportPDF.Name = "btnExportPDF";
+            this.btnExportPDF.Size = new System.Drawing.Size(142, 45);
+            this.btnExportPDF.TabIndex = 21;
+            this.btnExportPDF.Text = "تولید پی دی اف";
+            this.btnExportPDF.UseVisualStyleBackColor = true;
+            this.btnExportPDF.Click += new System.EventHandler(this.btnExportPDF_Click);
             // 
             // btnExportExcel
             // 
             this.btnExportExcel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExportExcel.Location = new System.Drawing.Point(302, 9);
+            this.btnExportExcel.Location = new System.Drawing.Point(460, 9);
             this.btnExportExcel.Name = "btnExportExcel";
             this.btnExportExcel.Size = new System.Drawing.Size(142, 45);
-            this.btnExportExcel.TabIndex = 18;
+            this.btnExportExcel.TabIndex = 20;
             this.btnExportExcel.Text = "تولید اکسل";
             this.btnExportExcel.UseVisualStyleBackColor = true;
             this.btnExportExcel.Click += new System.EventHandler(this.btnExportExcel_Click);
@@ -229,12 +231,14 @@
             this.btnNew.Name = "btnNew";
             this.btnNew.Size = new System.Drawing.Size(142, 45);
             this.btnNew.TabIndex = 15;
-            this.btnNew.Text = "نمایش";
+            this.btnNew.Text = "جدید";
             this.btnNew.UseVisualStyleBackColor = true;
             this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.btnSearch);
+            this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.txtFilter);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(3, 3);
@@ -242,24 +246,32 @@
             this.panel2.Size = new System.Drawing.Size(939, 50);
             this.panel2.TabIndex = 17;
             // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(619, 14);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = ">>>";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(865, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "جستجو";
+            // 
             // txtFilter
             // 
             this.txtFilter.Location = new System.Drawing.Point(700, 14);
             this.txtFilter.Name = "txtFilter";
             this.txtFilter.Size = new System.Drawing.Size(159, 22);
             this.txtFilter.TabIndex = 0;
-            this.txtFilter.TextChanged += new System.EventHandler(this.txtFilter_TextChanged);
-            // 
-            // btnExportPDF
-            // 
-            this.btnExportPDF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExportPDF.Location = new System.Drawing.Point(6, 9);
-            this.btnExportPDF.Name = "btnExportPDF";
-            this.btnExportPDF.Size = new System.Drawing.Size(142, 45);
-            this.btnExportPDF.TabIndex = 21;
-            this.btnExportPDF.Text = "تولید پی دی اف";
-            this.btnExportPDF.UseVisualStyleBackColor = true;
-            this.btnExportPDF.Click += new System.EventHandler(this.btnExportPDF_Click);
+            this.txtFilter.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtFilter_KeyDown);
             // 
             // frmMain
             // 
@@ -271,8 +283,9 @@
             this.Name = "frmMain";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.RightToLeftLayout = true;
-            this.Text = "Form1";
+            this.Text = "شرکه زهره البصره";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmMain_Paint);
             this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPassengers)).EndInit();
             this.panel1.ResumeLayout(false);
@@ -290,10 +303,9 @@
         private System.Windows.Forms.Button btnNew;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnEdit;
-        private System.Windows.Forms.Button btnExportExcel;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.TextBox txtFilter;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnExportExcel;
         private System.Windows.Forms.DataGridViewTextBoxColumn colID;
         private System.Windows.Forms.DataGridViewTextBoxColumn colName;
         private System.Windows.Forms.DataGridViewTextBoxColumn colFamily;
@@ -304,6 +316,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colExpiryDate;
         private System.Windows.Forms.DataGridViewTextBoxColumn colGender;
         private System.Windows.Forms.Button btnExportPDF;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- VisaX/frmMain.cs
+++ VisaX/frmMain.cs
@@ -36,7 +36,7 @@ namespace VisaX
             frmAddPassenger.ShowDialog();
             if (frmAddPassenger.DialogResult == DialogResult.OK)
             {
-                dgvPassengers.Refresh();
+                frmMain_Load(null, null);
             }
         }
 
@@ -46,105 +46,7 @@ namespace VisaX
             frmAddPassenger.ShowDialog();
             if (frmAddPassenger.DialogResult == DialogResult.OK)
             {
-                dgvPassengers.Update();
-                dgvPassengers.Refresh();
-            }
-        }
-
-        private void btnExportExcel_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.Filter = "Excel Documents (*.xls)|*.xls";
-            sfd.FileName = "Visa_Export.xls";
-            if (sfd.ShowDialog() == DialogResult.OK)
-            {
-                // Copy DataGridView results to clipboard
-                copyAlltoClipboard();
-
-                object misValue = System.Reflection.Missing.Value;
-                Excel.Application xlexcel = new Excel.Application();
-
-                //Excel.Application excel = new Excel.Application();
-                //Excel.Workbook wb = excel.Workbooks.Open(@"C:\Users\Mahmood\Desktop\Visa_Export3.xls");
-                //Excel.Worksheet xlMainWorkSheet = wb.Worksheets[1];
-                //xlMainWorkSheet.Cells.Range[1, 1].Value2 = "OK";
-
-                xlexcel.DisplayAlerts = false; // Without this you will get two confirm overwrite prompts
-                //Add workbook and worksheet
-                Excel.Workbook xlWorkBook = xlexcel.Workbooks.Add(misValue);
-                Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-
-                xlWorkSheet.Shapes.AddPicture(@"D:\Programing\VisaX\Files\Logo.jpg", MsoTriState.msoFalse, MsoTriState.msoCTrue, 50, 50, 300, 45);
-
-                // Format column D as text before pasting results, this was required for my data
-                Excel.Range rng = xlWorkSheet.get_Range("D:D").Cells;
-                rng.NumberFormat = "@";
-
-                // Paste clipboard results to worksheet range - Cells(row,col) and based on 1
-                Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[2, 2];
-                CR.Select();
-                // xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
-                xlWorkSheet.Paste(CR, Type.Missing);
-
-                Excel.Range yy = (Excel.Range)xlWorkSheet.Cells[2, 1];
-                yy.Value2 = "ردیف";
-
-                for (int j = 3; j < dgvPassengers.Columns.Count; j++)
-                {
-                    Excel.Range myRange = (Excel.Range)xlWorkSheet.Cells[j, 1];
-                    myRange.Value2 = j;
-                }
-
-
-                //// For some reason column A is always blank in the worksheet. ¯\_(ツ)_/¯
-                //// Delete blank column A and select cell A1
-                //Excel.Range delRng = xlWorkSheet.get_Range("A:A").Cells;
-                //delRng.Delete(Type.Missing);
-                //xlWorkSheet.get_Range("A1").Select();
-
-                // Save the excel file under the captured location from the SaveFileDialog
-                xlWorkBook.SaveAs(sfd.FileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
-                xlexcel.DisplayAlerts = true;
-                xlWorkBook.Close(true, misValue, misValue);
-                xlexcel.Quit();
-
-                releaseObject(xlWorkSheet);
-                releaseObject(xlWorkBook);
-                releaseObject(xlexcel);
-
-                // Clear Clipboard and DataGridView selection
-                Clipboard.Clear();
-                dgvPassengers.ClearSelection();
-
-                // Open the newly saved excel file
-                if (File.Exists(sfd.FileName))
-                    System.Diagnostics.Process.Start(sfd.FileName);
-            }
-        }
-
-        private void copyAlltoClipboard()
-        {
-            dgvPassengers.SelectAll();
-            DataObject dataObj = dgvPassengers.GetClipboardContent();
-            if (dataObj != null)
-                Clipboard.SetDataObject(dataObj);
-        }
-
-        private void releaseObject(object obj)
-        {
-            try
-            {
-                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
-                obj = null;
-            }
-            catch (Exception ex)
-            {
-                obj = null;
-                MessageBox.Show("Exception Occurred while releasing object " + ex.ToString());
-            }
-            finally
-            {
-                GC.Collect();
+                frmMain_Load(null, null);
             }
         }
 
@@ -155,131 +57,124 @@ namespace VisaX
                     dgvPassengers.Rows[i].DefaultCellStyle.BackColor = Color.WhiteSmoke;
         }
 
-        private void testExport(object sender, EventArgs e)
+        private void btnExportExcel_Click(object sender, EventArgs e)
         {
-            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
-            excel.Visible = true;
-            Microsoft.Office.Interop.Excel.Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
-            Microsoft.Office.Interop.Excel.Worksheet sheet1 = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Sheets[1];
-            int StartCol = 1;
-            int StartRow = 1;
-            int j = 0, i = 0;
+            string path = "./PassengerList.xls";
+            string absPath = Path.GetFullPath(path);
 
-            //Write Headers
-            for (j = 0; j < dgvPassengers.Columns.Count; j++)
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Documents (*.xls)|*.xls";
+            sfd.FileName = "Visa_Export.xls";
+            if (sfd.ShowDialog() == DialogResult.OK)
             {
-                Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[StartRow, StartCol + j];
-                myRange.Value2 = dgvPassengers.Columns[j].HeaderText;
-            }
+                Excel.Application excelApllication = null;
+                Excel.Workbook excelWorkBook = null;
+                Excel.Worksheet excelWorkSheet = null;
 
-            StartRow++;
+                excelApllication = new Excel.Application();
+                System.Threading.Thread.Sleep(2000);
+                excelWorkBook = excelApllication.Workbooks.Open(absPath, 0, true, 5, "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                excelWorkSheet = (Excel.Worksheet)excelWorkBook.Worksheets.get_Item(1);
 
-            //Write datagridview content
-            for (i = 0; i < dgvPassengers.Rows.Count; i++)
-                for (j = 0; j < dgvPassengers.Columns.Count; j++)
+                for (int i = 0; i < dgvPassengers.Rows.Count; i++)
                 {
-                    Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[StartRow + i, StartCol + j];
-                    myRange.Value2 = dgvPassengers[j, i].Value == null ? "" : dgvPassengers[j, i].Value;
-                }
-        }
+                    excelWorkSheet.Cells[i + 8, 8].Value = string.Format("{0} {1} {2}", dgvPassengers[1, i].Value, dgvPassengers[3, i].Value, dgvPassengers[2, i].Value);
+                    excelWorkSheet.Cells[i + 8, 7].Value = dgvPassengers[4, i].Value;
+                    excelWorkSheet.Cells[i + 8, 6].Value2 = (byte)dgvPassengers[8, i].Value == 0 ? "ذکر" : "انثی";
 
-        private void txtFilter_TextChanged(object sender, EventArgs e)
-        {
+                    //format error: Exception from HRESULT: 0x800A03EC
+                    excelWorkSheet.Cells[i + 8, 5].Value2 = dgvPassengers[5, i].Value;
+                    excelWorkSheet.Cells[i + 8, 4].Value2 = dgvPassengers[6, i].Value;
+                    excelWorkSheet.Cells[i + 8, 3].Value2 = dgvPassengers[7, i].Value;
+                }//for
 
-            dgvPassengers.DataSource = (dgvPassengers.DataSource as List<Passenger>).Where(p => p.Name.Contains(txtFilter.Text)).ToList<Passenger>();
-        }
+                excelWorkBook.SaveAs(sfd.FileName, Excel.XlFileFormat.xlWorkbookNormal);
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            string path = @"D:\Programing\VisaX\Documentation\test.xls";
-            Excel.Application excelApllication = null;
-            Excel.Workbook excelWorkBook = null;
-            Excel.Worksheet excelWorkSheet = null;
+                excelWorkBook.Close();
+                excelApllication.Quit();
 
-            excelApllication = new Excel.Application();
-            System.Threading.Thread.Sleep(2000);
-            excelWorkBook = excelApllication.Workbooks.Open(path, 0, true, 5, "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
-            excelWorkSheet = (Excel.Worksheet)excelWorkBook.Worksheets.get_Item(1);
-
-            for (int i = 0; i < dgvPassengers.Rows.Count; i++)
-            {
-                excelWorkSheet.Cells[i + 8, 8].Value = string.Format("{0} {1} {2}", dgvPassengers[1, i].Value, dgvPassengers[3, i].Value, dgvPassengers[2, i].Value);
-                excelWorkSheet.Cells[i + 8, 7].Value = dgvPassengers[4, i].Value;
-                excelWorkSheet.Cells[i + 8, 6].Value2 = (byte)dgvPassengers[8, i].Value == 0 ? "ذکر" : "انثی";
-
-                //format error: Exception from HRESULT: 0x800A03EC
-                excelWorkSheet.Cells[i + 8, 5].Value2 = dgvPassengers[5, i].Value;
-                excelWorkSheet.Cells[i + 8, 4].Value2 = dgvPassengers[6, i].Value;
-                excelWorkSheet.Cells[i + 8, 3].Value2 = dgvPassengers[7, i].Value;
-            }//for
-
-            path = @"D:\Programing\VisaX\Documentation\test2.xls";
-            excelWorkBook.SaveAs(path, Excel.XlFileFormat.xlWorkbookNormal);
-
-            excelWorkBook.Close();
-            excelApllication.Quit();
-
-            Marshal.FinalReleaseComObject(excelWorkSheet);
-            Marshal.FinalReleaseComObject(excelWorkBook);
-            Marshal.FinalReleaseComObject(excelApllication);
-            excelApllication = null;
-            excelWorkSheet = null;
-            //opens the created and saved Excel file
-            Process.Start(path);
+                Marshal.FinalReleaseComObject(excelWorkSheet);
+                Marshal.FinalReleaseComObject(excelWorkBook);
+                Marshal.FinalReleaseComObject(excelApllication);
+                excelApllication = null;
+                excelWorkSheet = null;
+                //Opens the created Excel file
+                Process.Start(sfd.FileName);
+            }
         }
 
         private void btnExportPDF_Click(object sender, EventArgs e)
         {
             // path to source file
-            String source = @"D:\Programing\VisaX\Documentation\InstaVisa.pdf";
+            String source = ".\\VisaForm.pdf";
             //create PdfReader object to read the source file
             PdfReader reader = new PdfReader(source);
-            //PdfStamper object to modify the content of the PDF
-            PdfStamper stamp = new PdfStamper(reader, new FileStream("d:/formfilled.pdf", FileMode.Create));
-            AcroFields form = stamp.AcroFields;
-            IDictionary<string, AcroFields.Item> fs = form.Fields;
-            /*
-            StringBuilder sb = new StringBuilder();
-            sb.Append("Possible values for form1[0].#subform[0].RadioButtonList[0]:");
-            sb.Append(Environment.NewLine);
-            string[] states = form.GetAppearanceStates("form1[0].#subform[0].RadioButtonList[0]");
 
-            for (int i = 0; i < states.Length - 1; i++)
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Adobe Acrobat Documents (*.pdf)|*.pdf";
+            sfd.FileName = "VisaApply.pdf";
+            if (sfd.ShowDialog() == DialogResult.OK)
             {
-                sb.Append(states[i]);
-                sb.Append(", ");
+                //PdfStamper object to modify the content of the PDF
+                PdfStamper stamp = new PdfStamper(reader, new FileStream(sfd.FileName, FileMode.Create));
+                AcroFields form = stamp.AcroFields;
+
+                Passenger p = (Passenger)dgvPassengers.CurrentRow.DataBoundItem;
+                form.SetField("form1[0].#subform[0].#field[0]", string.Format("{0} {1} {2}", p.Name, p.Father, p.Family));
+                //form.SetField("form1[0].#subform[0].#field[1]", "اسلام");
+                //Radio for Gender
+                form.SetField("form1[0].#subform[0].RadioButtonList[0]", (2 - p.Gender).ToString());
+                //form.SetField("form1[0].#subform[0].#field[2]", "ایرانیه");
+                //form.SetField("form1[0].#subform[0].#field[3]", "ایرانیه");
+                //form.SetField("form1[0].#subform[0].#field[9]", "ایران");
+                form.SetField("form1[0].#subform[0].#field[8]", p.BornDate.Year.ToString());
+                form.SetField("form1[0].#subform[0].#field[5]", p.BornDate.Month.ToString("00"));
+                form.SetField("form1[0].#subform[0].#field[6]", p.BornDate.Day.ToString("00"));
+                form.SetField("form1[0].#subform[0].#field[21]", p.PassportNum);
+
+                //Issue Date
+                form.SetField("form1[0].#subform[0].#field[25]", p.IssueDate.Year.ToString());
+                form.SetField("form1[0].#subform[0].#field[22]", p.IssueDate.Month.ToString("00"));
+                form.SetField("form1[0].#subform[0].#field[23]", p.IssueDate.Day.ToString("00"));
+
+                //Expiry Date
+                form.SetField("form1[0].#subform[0].#field[30]", p.ExpiryDate.Year.ToString());
+                form.SetField("form1[0].#subform[0].#field[27]", p.ExpiryDate.Month.ToString("00"));//Month
+                form.SetField("form1[0].#subform[0].#field[28]", p.ExpiryDate.Day.ToString("00"));//Day
+
+                stamp.Close();
+                Process.Start(sfd.FileName);
             }
-            sb.Append(states[states.Length - 1]);
-            MessageBox.Show(sb.ToString());
-            */
+        }
 
-            Passenger p = (Passenger)dgvPassengers.CurrentRow.DataBoundItem;
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            this.rowColor();
+        }
 
-            form.SetField("form1[0].#subform[0].#field[0]", string.Format("{0} {1} {2}", p.Name, p.Father, p.Family));
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var l = from p in ctx.Passengers where p.Family.Contains(txtFilter.Text) || p.Name.Contains(txtFilter.Text) ||
+                   p.Father.Contains(txtFilter.Text) || p.PassportNum.Contains(txtFilter.Text)  select p;
+            dgvPassengers.DataSource = l.ToList();
+        }
 
-            form.SetField("form1[0].#subform[0].#field[1]", "اسلام");
-            //Radio for Gender
-            form.SetField("form1[0].#subform[0].RadioButtonList[0]", (2- p.Gender).ToString());
-            form.SetField("form1[0].#subform[0].#field[2]", "ایرانیه");
-            form.SetField("form1[0].#subform[0].#field[3]", "ایرانیه");
-            form.SetField("form1[0].#subform[0].#field[9]", "ایران");
-            form.SetField("form1[0].#subform[0].#field[8]", p.BornDate.Year.ToString());
-            form.SetField("form1[0].#subform[0].#field[5]", p.BornDate.Month.ToString());
-            form.SetField("form1[0].#subform[0].#field[6]", p.BornDate.Day.ToString());
-            form.SetField("form1[0].#subform[0].#field[21]", p.PassportNum);
-            //Issue Date
-            form.SetField("form1[0].#subform[0].#field[25]", p.IssueDate.Year.ToString());
-            form.SetField("form1[0].#subform[0].#field[22]", p.IssueDate.Month.ToString());
-            form.SetField("form1[0].#subform[0].#field[23]", p.IssueDate.Day.ToString());
-            //Expiry Date
-            form.SetField("form1[0].#subform[0].#field[30]", p.ExpiryDate.Year.ToString());
-            form.SetField("form1[0].#subform[0].#field[27]", p.ExpiryDate.Month.ToString());
-            form.SetField("form1[0].#subform[0].#field[28]", p.ExpiryDate.Day.ToString());
+        private void dgvPassengers_RowsRemoved(object sender, DataGridViewRowsRemovedEventArgs e)
+        {
+            btnNew.Enabled = btnDelete.Enabled = btnEdit.Enabled = btnExportExcel.Enabled = btnExportPDF.Enabled =
+                dgvPassengers.Rows.Count != 0;
+        }
 
-            //for (int i = 0; i < 40; i++)
-            //    form.SetField(string.Format("form1[0].#subform[0].#field[{0}]", i), i.ToString());
-            stamp.Close();
-            System.Diagnostics.Process.Start("d:/formfilled.pdf");
+        private void dgvPassengers_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            btnNew.Enabled = btnDelete.Enabled = btnEdit.Enabled = btnExportExcel.Enabled = btnExportPDF.Enabled =
+                dgvPassengers.Rows.Count != 0;
+        }
+
+        private void txtFilter_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btnSearch_Click(null, null);
         }
     }
 }
